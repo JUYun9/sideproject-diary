253c5bd47b7d378f841e32953a456eb1980a1c84
KING <dbswjddn6@gmail.com>
2025-10-20 17:53:27 +0900

    CHORE: 최상위 디렉터리 생성

---
+++ sideproject1/src/main/java/hello/sideproject1/Entry.java
@@ -0,0 +1,29 @@
+package hello.sideproject1;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table
+public class Entry {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
